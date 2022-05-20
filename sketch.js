154f9fcecb30eb497a9f8443fbee2794b21fb7b0
sanskruti2010 <105872376+sanskruti2010@users.noreply.github.com>
2022-05-20 18:34:04 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,20,20)
+
+}
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown(RIGHT_ARROW)){
+    box.position.x=box.position.x+2
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    box.position.x=box.position.x-2
+  }
+  if (keyIsDown(UP_ARROW)){
+    box.position.y=box.position.y-2
+  }
+   if (keyIsDown(DOWN_ARROW)){
+    box.position.y=box.position.y+2
+  }
+  drawSprites();
+  
+ 
+}
+
+
+
+
